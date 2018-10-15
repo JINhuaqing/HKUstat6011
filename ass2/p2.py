8f0b399eb6301968a5cefabfb6204ed01e83c54d
JINhuaqing <kevinjin0423@gmail.com>
2018-10-15 07:55:12 -0400

    add python code for assignment 2

---
+++ ass2/p2.py
@@ -0,0 +1,63 @@
+# coding:utf8
+import numpy as np
+from scipy import stats
+from scipy.special import gamma
+import matplotlib.pyplot as plt
+from pathlib import Path
+import pickle
+
+
+np.random.seed(0)
+root = Path('./ass2/savedoc')
+if not root.is_dir():
+    root.mkdir()
+
+
+C = np.sqrt(np.pi/2)/gamma(1.5)
+t2 = stats.t(2)
+laplace = stats.laplace()
+plt.switch_backend('agg')
+
+# part (b)
+x = np.linspace(-10, 10, 1000)
+plt.figure(figsize=(20, 10))
+plt.title("t2 distribution with C vs laplace distribution")
+plt.plot(x, laplace.pdf(x), 'r', label='laplace distribution')
+plt.plot(x, C*t2.pdf(x), 'g', label='t2 distribution with C')
+plt.legend()
+plt.savefig(root/'p2b.jpg')
+plt.close()
+
+
+# part (c)
+num = 10000
+allnum = int(5e4)
+u01 = stats.uniform(0,1)
+therate = 1/C
+f = laplace.pdf
+g = t2.pdf
+gs = t2.rvs(allnum)
+us = u01.rvs(allnum)
+probs = f(gs)/C/g(gs)
+restsamples = gs[us<=probs]
+print(f'theoretical rejection rate is {1-therate:>10.3f}')
+print(f' experiment rejection rate is {1-len(restsamples)/allnum:>10.3f}')
+
+
+# part (d)
+gs1000 = gs[:1000]
+us1000 = us[:1000]
+probs1000 = probs[:1000]
+x1, x2 = gs1000[us1000<=probs1000], gs1000[us1000>probs1000]
+plt.figure(figsize=(20, 10))
+plt.xlim([-10, 10])
+plt.plot(x1, g(x1), 'o', label='accepted samples')
+plt.plot(x2, g(x2), 'x', label='rejected samples')
+plt.plot(x, laplace.pdf(x), 'r', label='laplace distribution')
+plt.plot(x, t2.pdf(x), 'g', label='t2 distribution')
+plt.legend()
+plt.savefig(root/'p2d.jpg')
+
+# part (e)
+rg = 10
+norm = stats.
---
+++ ass2/p3.py
@@ -0,0 +1,48 @@
+# coding:utf8
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+plt.switch_backend('agg')
+
+root = Path('./ass2/savedoc/')
+if not root.is_dir():
+    root.mkdir()
+
+# part (a)
+lognormrv = stats.lognorm(s=2, scale=1)
+gammarv = stats.gamma(a=2, scale=2)
+J = int(1e5)
+m = int(J/10)
+gsamples = lognormrv.rvs(J)
+ws = gammarv.pdf(gsamples)/lognormrv.pdf(gsamples)
+wsn = ws/ws.sum()
+fsamples = np.random.choice(gsamples, m, False, p=wsn)
+
+plt.figure(figsize=(20, 10))
+xx = np.linspace(0, 10, 100000)
+plt.title('gamma vs log normal vs SIR histogram')
+plt.plot(xx, gammarv.pdf(xx), 'r', label='gamma distribution')
+plt.plot(xx, lognormrv.pdf(xx), 'g', label='log normal distribution')
+plt.hist(fsamples, bins=50, density=True)
+plt.legend()
+if not (root/'p3a.jpg').is_file():
+    plt.savefig(root/'p3a.jpg')
+
+# part (b)
+xmode = 2
+a, b = 1, 3
+sncdf = stats.norm().cdf
+tvalue = gammarv.cdf(b) - gammarv.cdf(a)
+def h(x):
+    return np.log(1/4*x*np.exp(-x/2))
+
+def h2(x):
+    return -x**(-2) 
+def tmp(x):
+    return (x-xmode)/(-h2(xmode))**(-0.5)
+
+avalue = np.exp(h(xmode))*(3*np.pi/-h2(xmode))**(0.5)*(sncdf(tmp(b))-sncdf(tmp(a)))
+print(f'the true value is {tvalue:.5f}')
+print(f'the approx value is {avalue:.5f}')
